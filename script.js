adf8721837af32bb30f3db7e5b355a7bc70655a1
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-04 16:00:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+// resume generator
+// wrong function ~ have to write function totally of codewithharry building notes app
+//  function addmorewe(){
+//    console.log("i am working")
+//   let wefield = document.getElementsByClassName("wefield");
+//  let html =  wefield;
+//     html+= 
+//       `<div>
+//       <textarea type="text" class="form-control wefield mt-3"  placeholder="Enter here" rows="3"></textarea>
+//       </div>`;
+      
+//  let weobj = document.getElementById("we");
+//  let weaddbutton =  document.getElementById("weaddbutton");
+//   weobj.innerHTML = html ;
+//  }
